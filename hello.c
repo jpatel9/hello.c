127efcfd3d98b4fb8fe9ce773e2537606065e87d
jpatel <59984502+jpatel9@users.noreply.github.com>
2022-04-06 13:20:05 -0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -5,6 +5,7 @@ Date: 01/14/2020
 #include <stdio.h>
 
 int main(void)  {
+     
      printf("My name is : Jay Patel\n");
      printf("My major is Computer and Science");
      printf("Tyler Messerschmidt");
